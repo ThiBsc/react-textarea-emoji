e52e6c584a9ff37bccb95af6902489ce67c8f9dc
thibsc <thibaut.bsc@gmail.com>
2021-01-30 16:45:53 -0500

    Updates

---
+++ precache-manifest.d42f85d5138c035d54a747235a3fb46f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a894b0c899a45c2064a92019d3e62400",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fab59b5c7baf1d223f19",
+    "url": "./static/css/2.7ca58fb4.chunk.css"
+  },
+  {
+    "revision": "e8c74eae72f52285a355",
+    "url": "./static/css/main.b9217b08.chunk.css"
+  },
+  {
+    "revision": "fab59b5c7baf1d223f19",
+    "url": "./static/js/2.60ea098d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.60ea098d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8c74eae72f52285a355",
+    "url": "./static/js/main.1f6613b4.chunk.js"
+  },
+  {
+    "revision": "ec81f5ec7c29cc40b3ff",
+    "url": "./static/js/runtime-main.9db98d2c.js"
+  }
+]);
